ff27af1e0a6f342f640c263fe69087b18534f9d2
pingu342 <pingu342@users.noreply.github.com>
2021-06-06 16:20:06 +0900

    fix download url

--- build/aarch64/gcc6.4.0_glibc2.25_linux4.9.186/download.sh
+++ build/aarch64/gcc6.4.0_glibc2.25_linux4.9.186/download.sh
@@ -16,7 +16,7 @@ wget -nc http://ftpmirror.gnu.org/gcc/${GCC_VERSION}/${GCC_VERSION}.tar.gz
 wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/$LINUX_KERNEL_VERSION.tar.xz
 wget -nc http://ftpmirror.gnu.org/glibc/$GLIBC_VERSION.tar.xz
 wget -nc http://ftp.gnu.org/gnu/gmp/${GMP_VERSION}.tar.xz
-wget -nc https://www.mpfr.org/mpfr-current/${MPFR_VERSION}.tar.xz
+wget -nc https://www.mpfr.org/${MPFR_VERSION}/${MPFR_VERSION}.tar.xz
 wget -nc https://ftp.gnu.org/gnu/mpc/${MPC_VERSION}.tar.gz
 
 for f in *.tar*; do
